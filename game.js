bdac8aa25a5f4191db42abb4f349e8be27d2b307
stevey d <stphndcky22@gmail.com>
2022-02-15 15:58:49 -0800

    added variables for data storage

--- game.js
+++ game.js
@@ -1,3 +1,22 @@
-//Let's put an initial alert here
 
-window.alert("This is an alert! JavaScript is running!");
+
+
+
+// player names robot, gets starting attributes
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//enemy gets starting attributes
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+// fight variable
+var fight = function() {
+    window.alert ("Welcome to Robot Gladiators!");
+};
+
+// this calls the fight function
+fight();
