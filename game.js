1e0a21abd0206cadc321dee5b11de76b17ff2446
stevey d <stphndcky22@gmail.com>
2022-02-16 09:40:52 -0800

    created shop function, used switch statement for user choices, input shop request after battle

--- game.js
+++ game.js
@@ -91,6 +91,7 @@ var endGame = function() {
     // ask player to play again
     var playAgainConfirm = window.confirm("Your robot hungers for more destruction, satiate its appetite?");
     if (playAgainConfirm) {
+        
         // restart game
         startGame();
     }
@@ -98,6 +99,77 @@ var endGame = function() {
         window.alert("Return to the battlegrounds soon.");
     }
 };
+
+// shop function
+var shop = function() {
+    
+    // ask user what choice they will make
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Choose 'REFILL', 'UPGRADE', or 'LEAVE'.");
+    
+    // we will put the actions that will follow user response
+    switch (shopOptionPrompt) {
+        // if the user chooses 'refill'
+        case "REIFLL":
+        case "refill":
+            
+            // adding if statement so player needs money to shop
+            if (playerMoney >= 7) {
+            
+                window.alert("Refilling player's health by 20 for 7 money points.");
+
+            
+                // health will increase, money points will be lost
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney -7;
+            }
+
+            // else statement if user doesnt have enough money
+            else {
+                window.alert("Not enough cash!");
+            }
+
+            break;
+
+        // user chooses 'upgrade'
+        case "UPGRADE":
+        case "upgrade":
+            
+            // adding if statement for upgrade
+            if (playerMoney >= 7) {
+
+                window.alert("Upgrading attack power by 6 points for 7 money points.");
+
+                // attack power is increased, money points will be lost
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            }
+
+            // else statement if user doesnt have enough money
+            else {
+                window.alert("Not enough cash!");
+            }
+
+            break;
+
+        // user chooses to 'leave'
+        case "LEAVE":
+        case "leave":
+            window.alert("You leave the store.");
+
+            // user leaves so function will end
+            break;
+
+        // edge cases, this is for other inputs
+        default:
+            window.alert("You did not pick a valid option, please choose an option.");
+
+            // call shop function again at this point
+            shop();
+            break;    
+    }   
+};
+
+
 
 var startGame = function() {
 
@@ -108,14 +180,30 @@ var startGame = function() {
 
     for(var i = 0; i < enemyNames.length; i++) {
         if (playerHealth > 0) {
+
             // tells user what round it is
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            
             // new enemy is picked based on index of enemyNames array
             var pickedEnemyName = enemyNames[i];
+            
             // enemy health is reset at fight onset
             enemyHealth = 50;
+            
             // pickedEnemyName variable is put into fight function
             fight(pickedEnemyName);
+
+            // if there are still enemies in array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                
+                // ask player if they want to shop
+                var storeConfirm = window.confirm("Fight complete. Visit store?");
+
+                // if confirm, execute shop function
+                if (storeConfirm) {
+                    shop();
+                }
+            }
         }
         // game over message, code stops
         else {
@@ -127,6 +215,9 @@ var startGame = function() {
     // if loop ends and player is out of health or enemies run endGame () 
     endGame();
 };
+
+
+
 
 // call the startGame function
 startGame();
