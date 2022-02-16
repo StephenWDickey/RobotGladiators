70ab3d21932b8447bdb3bdeeb647687c5c914273
stevey d <stphndcky22@gmail.com>
2022-02-15 20:25:47 -0800

    added startGame and endGame functions

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-
+// Robot Gladiators
 
 
 
@@ -78,21 +78,57 @@ var fight = function(enemyName) {
 // this calls the fight function
 // fight();
 // We are replacing the above function call with a for loop
-for(var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        // tells user what round it is
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-        // new enemy is picked based on index of enemyNames array
-        var pickedEnemyName = enemyNames[i];
-        // enemy health is reset at fight onset
-        enemyHealth = 50;
-        // pickedEnemyName variable is put into fight function
-        fight(pickedEnemyName);
-    }
-}
 
-// Game States
-    // "WIN" - Player robot has defeated all enemy robots
-        // Fight all enemy robots
-        // Defeat each enemy robot
-    // "LOSE" - Player robot's health is zero or less
+var endGame = function() {
+    // if player is alive player wins
+    if (playerHealth > 0) {
+        window.alert("Your robot crushed its enemies and saw them driven before it! You have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("Though your robot fought bravely it has perished, it will be sold for scrap metal and you get no earnings. Goodbye.");
+    }
+
+    // ask player to play again
+    var playAgainConfirm = window.confirm("Your robot hungers for more destruction, satiate its appetite?");
+    if (playAgainConfirm) {
+        // restart game
+        startGame();
+    }
+    else {
+        window.alert("Return to the battlegrounds soon.");
+    }
+};
+
+var startGame = function() {
+
+    // reset user stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            // tells user what round it is
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            // new enemy is picked based on index of enemyNames array
+            var pickedEnemyName = enemyNames[i];
+            // enemy health is reset at fight onset
+            enemyHealth = 50;
+            // pickedEnemyName variable is put into fight function
+            fight(pickedEnemyName);
+        }
+        // game over message, code stops
+        else {
+            window.alert("Though your robot fought bravely it has perished, it will be sold for scrap metal and you get no earnings. Goodbye.");
+            break;
+        }
+    }
+    
+    // if loop ends and player is out of health or enemies run endGame () 
+    endGame();
+};
+
+// call the startGame function
+startGame();
+
+
