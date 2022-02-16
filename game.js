df9db06a934bbe88d88835949980c9699d3a7824
stevey d <stphndcky22@gmail.com>
2022-02-15 16:30:17 -0800

    updated fight function to include attacks and prompts checking health states

--- game.js
+++ game.js
@@ -15,8 +15,33 @@ var enemyAttack = 12;
 
 // fight variable
 var fight = function() {
+    // tells players they are starting round
     window.alert ("Welcome to Robot Gladiators!");
+    // subtract value of playerAttack from value of enemyHealth, update enemyHealth var
+    enemyHealth = enemyHealth - playerAttack;
+    // shows resulting message, note use of trailing and leading spaces
+    console.log (playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining." );
+    // check enemy health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else { 
+        window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
+    }
+    // subtract value of enemyAttack from playerHealth, update playerHealth var
+    playerHealth = playerHealth - enemyAttack;
+    // show resulting message
+    console.log (enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    // check player health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health remaining.");
+    }
 };
+
+
 
 // this calls the fight function
 fight();
